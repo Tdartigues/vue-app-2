5787cb2757f8df40fa7089f286ede86799972394
Tdartigues <73830061+Tdartigues@users.noreply.github.com>
2021-02-09 15:32:00 +0100

    Add Folder Docs

---
+++ docs/precache-manifest.fa9071d5cbfee467ff99b5321c23c00b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccb82610fd61c4fcdd54",
+    "url": "/vue-app-2/css/app.ddeedaa8.css"
+  },
+  {
+    "revision": "8ad84b68919eaaff93fb",
+    "url": "/vue-app-2/css/chunk-vendors.52eaf30c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-app-2/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "851a77f6cc81da868d6e7523dfcc4924",
+    "url": "/vue-app-2/index.html"
+  },
+  {
+    "revision": "1b716b345d53044f1cc5",
+    "url": "/vue-app-2/js/about.830eebe4.js"
+  },
+  {
+    "revision": "ccb82610fd61c4fcdd54",
+    "url": "/vue-app-2/js/app.5fe4fbfa.js"
+  },
+  {
+    "revision": "8ad84b68919eaaff93fb",
+    "url": "/vue-app-2/js/chunk-vendors.0d5e1992.js"
+  },
+  {
+    "revision": "a20c75afa1f0a984f0883b71519b2755",
+    "url": "/vue-app-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app-2/robots.txt"
+  }
+]);
